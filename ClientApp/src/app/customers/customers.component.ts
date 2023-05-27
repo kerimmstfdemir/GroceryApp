c436b36cd130186001ac6e1f4d11a59c68703186
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-05-27 14:02:24 +0300

    customer update operations were performed via modal on UI

--- ClientApp/src/app/customers/customers.component.ts
+++ ClientApp/src/app/customers/customers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormArray, Validators } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
   selector: 'customers',
@@ -11,6 +14,7 @@ export class CustomersComponent implements OnInit {
   customers: any[];
   form: FormGroup;
   selectedCustomer: any;
+  message: string = '';
 
   constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
 
@@ -29,30 +33,60 @@ export class CustomersComponent implements OnInit {
 
   createForm() {
     this.form = this.formBuilder.group({
-      fullname: [''],
-      gender: [''],
-      occupation: ['']
+      fullName: ['', Validators.required],
+      gender: ['', Validators.required],
+      occupation: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      website: [''],
+      subscribeToAds: [false],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      notes: ['']
     });
   }
 
   openEditCustomerModal(customer: any) {
     this.selectedCustomer = customer;
     this.form.patchValue({
-      fullname: customer.fullname,
+      fullname: customer.fullName,
       gender: customer.gender,
-      occupation: customer.occupation
+      occupation: customer.occupation,
+      birthDate: customer.birthDate,
+      email: customer.email,
+      website: customer.website,
+      subscribeToAds: customer.subscribeToAds,
+      address: customer.address,
+      city: customer.city,
+      notes: customer.notes
     });
   }
 
   updateCustomer() {
     const updatedCustomer = {
-      fullname: this.form.value.fullname,
+      fullName: this.form.value.fullName,
       gender: this.form.value.gender,
-      occupation: this.form.value.occupation
+      occupation: this.form.value.occupation,
+      birthDate: this.form.value.birthDate,
+      email: this.form.value.email,
+      website: this.form.value.website,
+      subscribeToAds: this.form.value.subscribeToAds,
+      address: this.form.value.address,
+      city: this.form.value.city,
+      notes: this.form.value.notes
     };
 
-    // TODO: HTTP isteği ile müşteri güncelleme işlemini gerçekleştirin
+    const apiUrl = `http://localhost:5000/api/customers/${this.selectedCustomer.id}`;
+  this.http.put(apiUrl, updatedCustomer).subscribe((response: any) => {
+    console.log('✓ Customer updated successfully');
+    this.message = '✓ Customer updated successfully';
+    this.fetchCustomers();
 
-    this.selectedCustomer = null;
+    setTimeout(() => {
+      this.message = '';
+    }, 3000);
+  });
+
+    $('#editModal').modal('hide');
   }
 }
